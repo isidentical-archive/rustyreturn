b29f51686e5e882bf6d7fe798f1d74f2f3ba2301
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-03-31 18:11:02 +0300

    improve tests

--- test_rustyreturn.py
+++ test_rustyreturn.py
@@ -1,18 +1,22 @@
 from rustyreturn import rlr
 
+
 @rlr
 def test(x, y):
     1 + 2
     x - y
     x + y
 
+
 @rlr
 def not_to_return(x, y):
     x + y
     a = x + y
 
+
 assert test(2, 3) == 5
 assert not_to_return(1, 2) is None
+
 
 @rlr
 def gt(x, y):
@@ -30,6 +34,7 @@ def gt(x, y):
                 False
     else:
         False
+
 
 assert gt(10, 2) is True
 assert gt(2, 10) is False
